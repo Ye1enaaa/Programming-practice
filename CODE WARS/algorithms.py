98386ea5dbc51e2c64d1cbc972f631bbf874eca6
EErickson <avilasonson@gmail.com>
2023-08-02 10:59:11 +0800

    problem 50% solve

--- CODE WARS/algorithms.py
+++ CODE WARS/algorithms.py
@@ -1,3 +1,4 @@
+import time
 #Write a Python program to insert items into a list in sorted order.
 #Original List:
 numbers = [25, 45, 36, 47, 69, 48, 68, 78, 14, 36]
@@ -17,5 +18,25 @@ def addNumber():
     print("Sorted List")
     print(numbers)
 
+# Write a Python program to create a queue and display all the members and size of the queue. Go to the editor
+# Expected Output:
+# Members of the queue:
+# 0 1 2 3
+# Size of the queue:
+# 4
 
-addNumber()
+#NOT DONE 
+
+queue = []
+
+def addOnQueue():
+    num = 0
+    number = num + 1
+    while True:
+        queue.append(number)
+        time.sleep(1)
+        print(queue)
+        print(len(queue))
+
+addOnQueue()
+#addNumber()
